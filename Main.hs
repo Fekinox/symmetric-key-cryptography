caf528b712b671c0aa01222d0585af302afa8935
William Owen Fowlkes III <williamowenfowlk@gmail.com>
2018-03-02 12:30:44 -0500

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -4,7 +4,9 @@ range l r x = (l <= x) && (x <= r)
 
 encode :: Char -> Int
 decode :: Int -> Char
-encode c = ord c - if (range 'a' 'z' c) then 0x60 else if (range 'A' 'Z' c) then 0x40 else 0x0
+encode c | range 'a' 'z' c = ord c - 0x60
+encode c | range 'A' 'Z' c = ord c - 0x40
+encode _ = 27
 decode n = chr $ fromIntegral (n+0x40)
 
 encodeTwo (x:y:str) = ((encode x)*100 + encode y) : (encodeTwo str)
@@ -17,3 +19,14 @@ decodeTwo = foldr (\lval ival -> let (l, r) = dcTwo lval in l:r:ival) ""
   
 encrypt key modulus = map (\n -> (n+key) `mod` modulus)
 decrypt key modulus = let inverse = modulus - key in encrypt inverse modulus
+
+encodeAndEncrypt key modulus = encrypt key modulus . encodeTwo
+decodeAndDecrypt key modulus = decodeTwo . decrypt key modulus
+
+splitIntoBlocks bs string | string /= [] = let (left, right) = splitAt bs string in left:(splitIntoBlocks bs right)
+splitIntoBlocks _ _ = []
+
+encodeBlocks blocksize string
+  | string == []  = []
+  | otherwise = map encodeBlock (splitIntoBlocks blocksize string)
+  where encodeBlock chars = sum $ zipWith (\c n -> (encode c)*(100^n)) (chars++(repeat '\0')) [blocksize-1,blocksize-2..0]
