3e20e482d6bc479ae7fcb9c86ee9d52013beb3e4
Fekinox <williamowenfowlk@gmail.com>
2018-03-07 06:59:21 -0500

    Final update to the project

--- Python/stream.py
+++ Python/stream.py
@@ -1,6 +1,8 @@
-# Stream Cipher
+# Stream-style Encryption
 
 import random
+
+# Encode letters as numbers, and convert back. Anything else is converted to the null case, 27.
 
 def encode(character):
 	# If character is uppercase...
@@ -16,8 +18,33 @@ def decode(number):
 	# Convert the number back into a letter
 	return chr(number+0x40) if number in range(1,26) else '-'
 
+def encodeString(string):
+	return [encode(c) for c in string]
+
+def decodeString(string):
+	res = ""
+	for n in string:
+		res += decode(n)
+	return res
+
+# Generates a randomized list. If called with the same key value, will return the same list- this is important
+
 def genRandomList(seed):
-    nums = list(range(1,27))
+    nums = list(range(1,28))
     random.seed(seed)
     random.shuffle(nums)
     return nums
+
+# Maps each encoded number to each number in the list, and back.
+
+def encrypt(key, string):
+	encodedString = encodeString(string)
+	randlist = genRandomList(key)
+	encryptedString = [randlist[i-1] for i in encodedString]
+	return decodeString(encryptedString)
+
+def decrypt(key, string):
+	encodedString = encodeString(string)
+	randlist = genRandomList(key)
+	decryptedString = [randlist.index(i)+1 for i in encodedString]
+	return decodeString(decryptedString)
